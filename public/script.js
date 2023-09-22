3f11c0a5e8afcf75126480bcaff31ce7d0f4254f
JesseIguess <32250628+BlueberryTech123@users.noreply.github.com>
2023-09-22 00:26:54 -0700

    .

--- public/script.js
+++ public/script.js
@@ -18,9 +18,50 @@ let errorWindow = `
     </div>
 </div>`
 
+const clickSound = new Audio("audio/click.wav");
+const keySound = new Audio("audio/key.wav");
+const errorSound = new Audio("audio/error.mp3");
+
 addEventListener("error", (event, source, line, col, error) => {
     displayError(`${event.message}\nLine: ${line}\nSource: ${source}`);
 });
+
+addEventListener("click", (event) => {
+    clickSound.currentTime = 0;
+    clickSound.play();
+});
+addEventListener("keypress", (event) => {
+    keySound.currentTime = 0;
+    keySound.play();
+})
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + value + ";";
+}
+function getCookie(name) {
+    let namePrefix = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(namePrefix) == 0) {
+            return c.substring(namePrefix.length, c.length);
+        }
+    }
+    return "";
+}
+function clearCookies(name) {
+    document.cookie = null;
+}
+
+function storeUser(username, password) {
+    clearCookies();
+    setCookie("username", username);
+    setCookie("password", password);
+}
 
 function displayError(error) {
     let window = document.querySelector("#error-window");
@@ -31,6 +72,7 @@ function displayError(error) {
         document.body.innerHTML += errorWindow;
         window = document.querySelector("#error-window");
     }
+    errorSound.play();
     document.getElementById("error").innerText = error;
     
 }
@@ -39,31 +81,92 @@ function closeError() {
     window.classList.add("hidden");
 }
 
+function displayWindow(id) {
+    document.querySelector(`#${id}`).classList.remove("hidden");
+}
+function closeWindow(id) {
+    document.querySelector(`#${id}`).classList.add("hidden");
+}
+
+
+function validate() {
+    closeWindow("loading");
+
+    if (getCookie("username") == "" || getCookie("password") == "") {
+        closeWindow("chat");
+        displayWindow("login");
+    }
+    else {
+        displayWindow("chat");
+        closeWindow("login");
+    }
+}
+function updateUserCookies(username, password) {
+    clearCookies();
+    setCookie("username", username);
+    setCookie("password", password);
+}
+
 function signOn() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    
+    displayWindow("loading");
 
     $.post("/signon", {username: username, password: password}, (data, status) => {
         if (data.success) {
-            window.location.href = "balls";
+            updateUserCookies(username, password);
+            validate();
         }
         else {
             displayError(data.message);
+            closeWindow("loading");
         }
     });
     return false;
+}
+function logOut() {
+    document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    document.cookie = "password=;expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    validate();
 }
 function createAccount() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    displayWindow("loading");
+
     $.post("/createaccount", {username: username, password: password}, (data, status) => {
         if (data.success) {
-            window.location.href = "balls";
+            updateUserCookies(username, password);
+            validate();
         }
         else {
             displayError(data.message);
+            closeWindow("loading");
         }
     });
     return false;
+}
+
+function createChat() {
+    const username = getCookie("username");
+    const password = getCookie("password");
+    const name = document.getElementById("add-input").value;
+
+    displayError(name);
+
+    displayWindow("loading");
+    alert("balls");
+
+    $.post("/createchat", {username: username, password: password, name: name}), (data, status) => {
+        alert("hi");
+        if (data.success) {
+            closeWindow("loading");
+        }
+        else {
+            displayError(data.message);
+            closeWindow("loading");
+        }
+    }
 }
--- server.mjs
+++ server.mjs
@@ -13,13 +13,25 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 const __dirname = path.dirname(require("url").fileURLToPath(import.meta.url));
 
-const bannedWords = ["izbica", "izbice", "serb", "yugoslav", "milosevic", "slobodan", "radovan", "karadzic", "ratko", "biljana", "biljana", "plavsic"]
+const bannedWords = ["izbica", "izbice", "serb", "yugoslav", "milosevic", "slobodan", "radovan", "karadzic", "ratko", "biljana", "plavsic", "srpska", "srpski"]
 
 const app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+let deviations = 0;
+
+function generateInvite() {
+    deviations++;
+    if (deviations == 68419) {
+        deviations = 0;
+    }
+    let time = `${new Date().toString()} ${deviations} i love oily black men <3 我爱油腻腻的黑人`;
+    let hash = crypto.createHash("md5").update(time).digest("hex").substring(0, 5);
+    return hash;
+}
 
 function hash(data) {
     return crypto.createHash('md5').update("i_hate_diggers_5967_" + data).digest("hex");
@@ -46,6 +58,31 @@ async function createAccount(username, password) {
         };
     }
 
+    if (password.length < 5) {
+        if (password.length <= 2) {
+            return {
+                message: "That password tho",
+                success: false
+            };
+        }
+        return {
+            message: "Password has to be at least 5 characters long",
+            success: false
+        };
+    }
+    if (!password.includes("ALBANIA")) {
+        return {
+            message: "Password must praise Albania in all caps (eg. password123ALBANIABEST)",
+            success: false
+        };
+    }
+    if (!isAppropriate(password)) {
+        return {
+            message: "Your password cannot reference Serbia",
+            success: false
+        };
+    }
+
     const {data, error} = await supabase.from("users").insert({
         username: username,
         password: hash(password),
@@ -63,7 +100,7 @@ async function createAccount(username, password) {
     };
 }
 async function authenticate(username, password) {
-    const {data, error} = await supabase.from("users").select().eq("username", username);
+    const {data, error} = await supabase.from("users").select().eq("username", username).eq("password", hash(password));
 
     if (error) {
         return {
@@ -73,7 +110,7 @@ async function authenticate(username, password) {
     }
     else if (data.length == 0) {
         return {
-            message: "User doesn't exist",
+            message: "Invalid credentials",
             success: false
         }
     }
@@ -86,14 +123,15 @@ async function authenticate(username, password) {
 
 
 async function createChat(username, password, name) {
-    const authMessage = await authenticate(username, password);
+    const auth = await authenticate(username, password);
 
-    if (!authMessage.success) return authMessage;
+    if (!auth.success) return auth;
 
-    const uid = authMessage.data["id"];
+    const uid = auth.message.id;
 
     const {data, error} = await supabase.from("chats").insert({
         name: name,
+        invite: generateInvite(),
         members: [uid],
         moderators: [uid],
         owner: uid,
@@ -125,6 +163,12 @@ app.post("/signon", async function(req, res) {
 });
 app.post("/createaccount", async function(req, res) {
     let message = await createAccount(req.body.username, req.body.password);
+    console.log(`Create Account: ${JSON.stringify(message)}`);
+    res.json(message);
+});
+app.post("/createchat", async function(req, res) {
+    let message = await createChat(req.body.username, req.body.password, req.res.name);
+    console.log(`Create Chat: ${JSON.stringify(message)}`);
     res.json(message);
 });
 
